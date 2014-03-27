371d92477a6b4d23e157b7d18a3b8df641a35e4a
SilSantana <sil_s_s@yahoo.com.br>
2014-03-27 11:52:39 +0800

    add name

--- ListNames/Form1.vb
+++ ListNames/Form1.vb
@@ -1,12 +1,9 @@
 ﻿Public Class Form1
 
-    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
-
-
-
-    End Sub
+ 
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ListBox1.Items.Add("LOPA")
+        ListBox1.Items.Add("Silvana")
     End Sub
 End Class

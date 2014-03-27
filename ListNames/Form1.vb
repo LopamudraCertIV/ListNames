4378776b4e799418aa01d09134874ec3ff8657bb
Lopa <lopamudralenka12@gmail.com>
2014-03-27 11:40:02 +0800

    list names

---
+++ ListNames/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListBox1.Items.Add("LOPA")
+    End Sub
+End Class
